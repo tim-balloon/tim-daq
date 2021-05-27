cd0897941d4969bde3894b95946c4f4c32cda929
James Aguirre <jaguirre@sas.upenn.edu>
2021-05-27 20:58:07 +0000

    updated generic serial parsing

--- ParseSerial.py
+++ ParseSerial.py
@@ -1,5 +1,10 @@
 import numpy as np
 import serial 
+import struct
+
+def hexToFloat(value): #convert hexadecimal to floating point for x,y,z readings   
+    return struct.unpack('!f', bytes.fromhex(value))[0]
+
 
 def ParseSerial(ser, header, splitline, sentence_length, Nbytes):
 
@@ -19,6 +24,7 @@ def ParseSerial(ser, header, splitline, sentence_length, Nbytes):
 
         if header in buf:
             if sentence != '' and len(sentence) == sentence_length:
+                
                 split_result = [sentence[i:j] for i, j in zip([0] + splitline, splitline + [None])]
                 #print(split_result)
                 print(int(split_result[7],16), # seq num
@@ -34,3 +40,43 @@ def ParseSerial(ser, header, splitline, sentence_length, Nbytes):
         cnt += 1
         
     return
+
+def ReadSentence(ser, header, splitline, sentence_length):
+
+    buf = ''
+    sentence = ''
+    
+    reading_sentence = False
+    sentence_complete = False
+    
+    while not sentence_complete:
+        
+        
+        
+        if len(sentence) == sentence_length - len(header):
+            
+            sentence_complete = True        
+            split_result = [sentence[i:j] for i, j in zip([0] + splitline, splitline + [None])]
+            
+            
+        
+        else:
+            onebyte = ser.read().hex() # to be safe, should set a timeout here, probably
+            
+            buf += onebyte 
+            
+            if reading_sentence: # if start bytes found on last pass
+                # start acculumlating the data 
+                sentence += onebyte
+                
+            if header in buf:
+                
+                reading_sentence = True
+                
+       
+    return (int(split_result[7],16), # seq numint(split_result[0],16), 
+            hexToFloat(split_result[0]), # x
+            hexToFloat(split_result[1]), # y
+            hexToFloat(split_result[2]), # z
+            int(split_result[6],16), # status
+            int(split_result[8],16)) # temperature
