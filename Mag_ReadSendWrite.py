3ef654e9175eee6f061a198dfe0aa7ef7bff84b3
James Aguirre <jaguirre@sas.upenn.edu>
2021-06-17 16:16:08 +0000

    Update to latest version

--- Mag_ReadSendWrite.py
+++ Mag_ReadSendWrite.py
@@ -12,13 +12,14 @@ import time
 import sys
 import h5py
 import os
+import glob
 
 
 # In[2]:
 
 
 #Starting with Influx 2.0, authorization tokens are necessary to access databases.
-#The token can be accessed under the "Data" tab in the Influx UI.
+#The token can be found under the "Data" tab in the Influx UI
 
 #Stringing together query access
 INFLUX_TOKEN='AjsrNgY_k97FMvgfCsgc2tPTx-lOVM-aYaCMjymNVIWpoSCkYh7H4AqIV9pLQHHk07zJa5pxTn4lo-3Ashwu5Q=='
@@ -53,27 +54,50 @@ splitline_mag = [4,8,12]
 sentence_length_mag = 14
 
 
-# In[6]:
+# In[5]:
 
 
-#Number of data samples in each save file. Divide by 100 for seconds. (Magnetometer currently set to 20hz)
-N = 585 * 60
+#Number of data samples in each save file. Divide by 100 for seconds. (Magnetometer currently set to 10hz)
+#N = 585 * 60
+N = 18000
 dataCount = 0
 
 
-# In[7]:
+# In[6]:
 
 
 #Creating HDF5 datasets... maybe convert to for loop
 timestr = time.strftime("%Y%m%d-%H%M%S")
-directory = time.strftime("Mag-%Y-%m-%d")
-os.mkdir("/home/user/tim-daq/data/"+directory)
-magfile = h5py.File("/home/user/tim-daq/data/"+directory+"/Mag-"+timestr+".hdf5", "w", libver = 'latest')
+datestamp = time.strftime("%Y-%m-%d-Mag")
+
+#First check if there is a folder with current date. If there isn't, create one.
+if os.path.isdir("/home/user/tim-daq/data/"+datestamp) == 0:
+    os.mkdir("/home/user/tim-daq/data/"+datestamp)
+
+#Then check if the folder is empty. If it's empty, the first file number will start at 1.
+#If the folder is not empty, check the file number of the last edited file, and add 1 to it for the new file. 
+if len(os.listdir("/home/user/tim-daq/data/"+datestamp)) == 0:
+    file_number = 1
+    file_number_string =  str(1)
+    
+else:
+    file_list = glob.glob("/home/user/tim-daq/data/"+datestamp+"/*.hdf5")
+    latest_file = max(file_list, key=os.path.getctime)
+    file_number = int(latest_file[-10:-5]) + 1
+    file_number_string =  str(file_number)
+    
+
+magfile = h5py.File("/home/user/tim-daq/data/"+datestamp+"/Mag_"+timestr+'_'+file_number_string.zfill(5)+".hdf5", "w", libver = 'latest')
 x_dset = magfile.create_dataset("x", (N,), dtype='float', chunks = True, maxshape = (None,))
 y_dset = magfile.create_dataset("y", (N,), dtype='float', chunks = True, maxshape = (None,))
 z_dset = magfile.create_dataset("z", (N,), dtype='float', chunks = True, maxshape = (None,))
 time_dset = magfile.create_dataset("time", (N,), dtype='int64', chunks = True, maxshape = (None,))
 #filesnames_dset = magfile.create_dataset("filenames",(2,),dtype='', chunks= True, maxshape = (None,))
+index_dset = magfile.create_dataset("index", (1,), dtype='int64', chunks = True, maxshape = (None,))
+filenames_dset = magfile.create_dataset("filenames", (2,), dtype='int64', chunks = True, maxshape = (None,))
+
+filenames_dset[0] = file_number
+filenames_dset[1] = file_number + 1
 
 
 magfile.swmr_mode = True
@@ -87,7 +111,7 @@ while True:
     x_buff, y_buff, z_buff = ReadSentence_Mag(mag, header_mag, splitline_mag, sentence_length_mag) 
     #print(x_buff)
     current_point_time = int(round(time.time() * 1000000000))
-    directory = time.strftime("Mag-%Y-%m-%d")
+    directory = time.strftime("%Y-%m-%d-Mag")
     current_line = '{measurement},location={location} mag_x={x},mag_y={y},mag_z={z} {timestamp}'.format(measurement=measurement_name,location=location_tag, x=x_buff,y=y_buff,z=z_buff, timestamp=current_point_time)       
     #print(current_line)
     r = requests.post(QUERY_URI, data=current_line, headers=headers)
@@ -96,11 +120,28 @@ while True:
     if dataCount >= N:
         magfile.close()
         current_timestr = time.strftime("%Y%m%d-%H%M%S")
-        magfile = h5py.File("/home/user/tim-daq/data/"+directory+"/Mag-"+current_timestr+".hdf5", "w", libver = 'latest')
+        datestamp = time.strftime("%Y-%m-%d-Mag")
+        
+        if os.path.isdir("/home/user/tim-daq/data/"+datestamp) == 0:
+            os.mkdir("/home/user/tim-daq/data/"+datestamp)  
+        
+        file_list = glob.glob("/home/user/tim-daq/data/"+datestamp+"/*.hdf5")
+        latest_file = max(file_list, key=os.path.getctime)
+        file_number = int(latest_file[-10:-5]) + 1
+        file_number_string =  str(file_number)
+        
+        
+        magfile = h5py.File("/home/user/tim-daq/data/"+datestamp+"/Mag_"+current_timestr+'_'+file_number_string.zfill(5)+".hdf5", "w", libver = 'latest')
         x_dset = magfile.create_dataset("x", (N,), dtype='float', chunks = True, maxshape = (None,))
         y_dset = magfile.create_dataset("y", (N,), dtype='float', chunks = True, maxshape = (None,))
         z_dset = magfile.create_dataset("z", (N,), dtype='float', chunks = True, maxshape = (None,))
         time_dset = magfile.create_dataset("time", (N,), dtype='int64', chunks = True, maxshape = (None,))
+        index_dset = magfile.create_dataset("index", (1,), dtype='int64', chunks = True, maxshape = (None,))
+        filenames_dset = magfile.create_dataset("filenames", (2,), dtype='int64', chunks = True, maxshape = (None,))
+        
+        filenames_dset[0] = file_number
+        filenames_dset[1] = file_number + 1
+        
         magfile.swmr_mode = True
         dataCount = 0
    
@@ -110,11 +151,13 @@ while True:
         y_dset[dataCount] = y_buff
         z_dset[dataCount] = z_buff
         time_dset[dataCount] = current_point_time
+        index_dset[0] = dataCount
         
         x_dset.flush()
         y_dset.flush()
         z_dset.flush()
         time_dset.flush()
+        index_dset.flush()
         dataCount += 1
 
 
